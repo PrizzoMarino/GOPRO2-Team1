b9b04d011cda47dbfaba39a883f467a34fded925
Diego Ovalle <diegoandresovalle2005@gmail.com>
2025-11-03 11:36:04 +0100

    MultiplayerSetup
    Removed the version 5.6 of the project since it may give some issues while saving files and folders.
    Removed the Advanced Session plug-in which we can't use.
    Integrated the setup to start building the OnlineSubsystem using c++

---
+++ GOPRO2_Team1/Source/GOPRO2_Team1/Private/Multiplayer/MyGameInstance.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Multiplayer/MyGameInstance.h"
+#include "OnlineSubsystem.h"
+#include "OnlineSessionSettings.h"
+
+UMyGameInstance::UMyGameInstance()
+{
+	
+}
+
+void UMyGameInstance::Init()
+{
+	Super::Init();
+	// Custom initialization code can be added here
+}
