50cd8f330ce7617166f8f82939fa878acee9ab18
Diego Ovalle <diegoandresovalle2005@gmail.com>
2025-11-04 13:59:47 +0100

    Upgrades
    Renamed and Polished every function
    Allow Players to change Name of Server
    Allow Players to Refresh Server List Properly

--- GOPRO2_Team1/Source/GOPRO2_Team1/Public/Multiplayer/MyGameInstance.h
+++ GOPRO2_Team1/Source/GOPRO2_Team1/Public/Multiplayer/MyGameInstance.h
@@ -7,9 +7,29 @@
 #include "Interfaces/OnlineSessionInterface.h"
 #include "MyGameInstance.generated.h"
 
-/**
- * 
- */
+
+USTRUCT(BlueprintType)
+struct FServerInfo
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(BlueprintReadOnly)
+		FString ServerName;
+	UPROPERTY(BlueprintReadOnly)
+		FString PlayerCountStr;
+	UPROPERTY(BlueprintReadOnly)
+		int32 CurrentPlayers;
+	UPROPERTY(BlueprintReadOnly)
+		int32 MaxPlayers;
+
+	void SetPlayerCount() 
+	{
+		PlayerCountStr = FString::FromInt(CurrentPlayers) + "/" + FString::FromInt(MaxPlayers);
+	}
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FServerDel, FServerInfo, ServerListDel);
+
 UCLASS()
 class GOPRO2_TEAM1_API UMyGameInstance : public UGameInstance
 {
@@ -20,6 +40,9 @@ public:
 	UMyGameInstance();
 
 protected:
+
+	UPROPERTY(BlueprintAssignable)
+	FServerDel ServerListDel;
 	
 	IOnlineSessionPtr SessionInterface;
 
@@ -32,10 +55,10 @@ protected:
 	virtual void OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
 	
 	
-	UFUNCTION(BlueprintCallable, Category = "Multiplayer")
-	void CreateSession();
+	UFUNCTION(BlueprintCallable)
+	void CreateSession(FString ServerName, FString HostName);
 	
-	UFUNCTION(BlueprintCallable, Category = "Multiplayer")
-	void JoinSession();
+	UFUNCTION(BlueprintCallable)
+	void FindServers();
 	
 };
